c1fe0af15ee76b5d68bfe1d52e6c68f7a562b625
Kunle Oshiyoye <mgen.glder@gmail.com>
2020-07-29 21:09:44 -0400

    Beginnings of getting unit tests to start working

---
+++ tests/unit/home.spec.ts
@@ -0,0 +1,27 @@
+import { expect } from 'chai'
+import { shallowMount, mount } from '@vue/test-utils'
+import Home from '@/views/Home.vue'
+import firebase from '@firebase/testing'
+import { IDBFactory, IDBKeyRange} from 'shelving-mock-indexeddb'
+
+// TODO: Create a declaration file for this dependency, so typescript won't complain anymore
+// global.window.firebase = {}
+// @ts-ignore
+window.indexedDB = new IDBFactory()
+window.IDBKeyRange = IDBKeyRange
+describe('HelloWorld.vue', async () => {
+  // await firebase.loadFirestoreRules({projectId: 'testId', rules: ''})
+  let fakeFirebase = await firebase.initializeTestApp({
+    projectId: "my-test-project",
+    auth: { uid: "alice", email: "alice@example.com" }
+  });
+  it('renders props.msg when passed', () => {
+    const msg = 'new message'
+    // const subject = shallowMount(Home, {
+
+    // })
+    // console.log(subject.html())
+    console.log(fakeFirebase);
+    expect(true).to.be.true;
+  })
+})
