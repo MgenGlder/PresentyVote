9ba30f6c2478b30de94d5eba60263a8346d28982
Kunle Oshiyoye <mgen.glder@gmail.com>
2020-08-09 18:41:59 -0400

    Comment out unneeded typescript declaration

--- types/shelving-mock-indexeddb/index.d.ts
+++ types/shelving-mock-indexeddb/index.d.ts
@@ -1,1 +1,2 @@
-declare module 'shelving-mock-indexeddb'
+// No longer needed, but might need to set up own declarations down the line so this might be a good example.
+// declare module 'shelving-mock-indexeddb'
